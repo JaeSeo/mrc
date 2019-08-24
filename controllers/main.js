523f6e185f36f0671ff98df4cfcb9c2cbab9598e
jaehyes@JAEHYES.COM <jaehyes@JAEHYES.COM>
2019-08-24 17:14:02 +0900

    first

---
+++ controllers/main.js
@@ -0,0 +1,11 @@
+exports.getIndex = (req, res, next) => {
+    res.render('index');
+};
+
+exports.postMessages = (req, res, next) => {
+    const phone = req.body.phone;
+    const messages = req.body.messages;
+    console.log(phone);
+    console.log(messages);
+    res.render('index');
+};
